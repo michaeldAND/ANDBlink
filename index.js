0b8a3c0f578127af3836e770e2a03f7e2a02f6c5
Michael Drennan <michaeldrennan@vh44yjkywn.home>
2022-06-20 12:17:35 +0100

    set up basic notifaction with dummy logo

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Notification } = require('electron');
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: './Logo.png',
   });
 
   win.loadFile('index.html');
 };
 
-app.whenReady().then(() => {
-  createWindow();
-});
+function createNotification(title, body) {
+  const breakNotification = new Notification({ title, body });
+  breakNotification.show();
+}
 
 app.whenReady().then(() => {
   createWindow();
-
+  createNotification('This is a title', 'This is a body');
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
