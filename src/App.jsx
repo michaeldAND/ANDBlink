dc0bee352e20c694a8ed0f3cb9c086ecee6766e8
Dylan Wright <dylanwright@WDN4LC437D.local>
2022-06-21 14:13:06 +0100

    Adding accordion example

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import React, { useState } from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import { Checkbox } from 'semantic-ui-react';
+import { Checkbox, Accordion } from 'semantic-ui-react';
 
 function App() {
   const [scheduleBreaks, setScheduleBreaks] = useState(0);
 
+  const scheduleBreakPanel = [
+    {
+      title: 'Schedule break',
+      content: [
+        'Schedule Break content',
+      ],
+    },
+  ];
+
   return (
 
-    <Checkbox toggle onClick={() => setScheduleBreaks(scheduleBreaks)}>
-      Toggle
-    </Checkbox>
-
+    <>
+      <Checkbox toggle onClick={() => setScheduleBreaks(scheduleBreaks)}>
+        Toggle
+      </Checkbox>
+      <Accordion>
+        <Accordion fluid styled defaultActiveIndex={0} panels={scheduleBreakPanel} />
+      </Accordion>
+    </>
   );
 }
 
