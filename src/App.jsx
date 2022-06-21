8d1432867d7d9d745a6b2ce0ca9c18a4712f3f6a
Dylan Wright <dylanwright@WDN4LC437D.local>
2022-06-21 12:37:52 +0100

    Remove React default styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,16 @@
 import React, { useState } from 'react';
-import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 import { Checkbox } from 'semantic-ui-react';
-import logo from './logo.svg';
 
 function App() {
   const [scheduleBreaks, setScheduleBreaks] = useState(0);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <Checkbox toggle onClick={() => setScheduleBreaks(scheduleBreaks)}>
-          Toggle
-        </Checkbox>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Checkbox toggle onClick={() => setScheduleBreaks(scheduleBreaks)}>
+      Toggle
+    </Checkbox>
+
   );
 }
 
