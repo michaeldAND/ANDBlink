2f91cc01020e55c63f1363bef7d7fb22193f2837
Joe <joe@K4DY4VRHC0.local>
2022-06-23 11:59:19 +0100

    set up second view

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 import {
-  Checkbox, Icon, Menu, Segment,
+  Checkbox, GridColumn, GridRow, Icon, Menu, Segment, Image,
 } from 'semantic-ui-react';
+import sadFace from './sad_face.png';
 
 function App() {
   const [scheduleBreaks, setScheduleBreaks] = useState(false);
@@ -61,7 +62,25 @@ function App() {
       />
     </Segment>
   </Segment>,
-    'Daily check-in': <Segment>number 2</Segment>,
+    'Daily check-in':
+  <Segment>
+    <p>
+      It&apos;s important to check-in daily to understand how you&apos;re feeling
+      and how you can shift your day to suit your needs.
+    </p>
+    <p>How are you feeling today?</p>
+    <GridRow>
+      <GridColumn>
+        <Image src={sadFace} size="small" />
+      </GridColumn>
+      <GridColumn>
+        <Image src="/neutral_face" size="small" />
+      </GridColumn>
+      <GridColumn>
+        <Image src="/sad_face" size="small" />
+      </GridColumn>
+    </GridRow>
+  </Segment>,
   };
   return (
     <>
